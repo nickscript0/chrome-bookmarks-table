3329a9f65197719c9e62f531514dcba5a783a5d8
nickscript0 <nocl123@gmail.com>
2015-08-22 17:47:01 -0600

    refactor: mv js to src folder

--- background.js
+++
@@ -1,5 +0,0 @@
-// Open main.html on icon click
-chrome.browserAction.onClicked.addListener( function() {
-    chrome.tabs.create({'url': chrome.extension.getURL('main.html')}, function(tab) {
-    });
-});
---
+++ src/background.js
@@ -0,0 +1,6 @@
+// Open main.html on icon click
+chrome.browserAction.onClicked.addListener(function() {
+  chrome.tabs.create({
+    'url': chrome.extension.getURL('main.html')
+  }, function(tab) {});
+});
