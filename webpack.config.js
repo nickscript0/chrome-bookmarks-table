93f03149c5611497ba7a0fd42ea89b44efb5aea2
nickscript0 <nickscript0@users.noreply.github.com>
2017-07-22 08:00:54 -0600

    add typescript and webpack build

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),  
+        filename: 'bundle.js',
+        publicPath: '/dist/'
+    },
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    },
+    module: {
+        loaders: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            { test: /\.tsx?$/, loader: 'ts-loader' }
+        ]
+    }
+}
