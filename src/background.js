93c90b92a74f51f6bc74adb08dab0310b07fa8ff
nickscript0 <nickscript0@users.noreply.github.com>
2021-09-18 16:20:42 -0300

    remove unnecessary permissions, apparently no tab permissions are required to create a new tab and run a local html/js page

--- src/background.js
+++ src/background.js
@@ -1,14 +1,6 @@
 // Open main.html on icon click
 chrome.action.onClicked.addListener((_tab) => {
-    chrome.tabs.create(
-        {
-            url: 'main.html',
-        },
-        (tab) => {
-            chrome.scripting.executeScript({
-                target: { tabId: tab.id },
-                files: ['src/main.js'],
-            });
-        }
-    );
+    chrome.tabs.create({
+        url: 'main.html',
+    });
 });
